cf33c011eed50eb58ea84a56bd03ce44e08ea9f3
garus_ae <garus.ae@weldbook.ru>
2021-12-19 20:41:02 +0300

    task weather api

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { tap } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'taskapi';
+export class AppComponent implements OnInit {
+  title = 'weatherApi';
+  cities: any;
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {}
+  // ipKey 7f7cc2ea-568f-4dae-9358-12518c1b7c7d
+
+  onClick() {
+    const domain = 'http://api.airvisual.com';
+    const endpoint = 'v2/cities';
+    const APIKey = '7f7cc2ea-568f-4dae-9358-12518c1b7c7d';
+    const APIparams = {
+      state: 'California',
+      country: 'USA',
+      key: APIKey,
+    };
+
+    const url = `${domain}${endpoint}`;
+    this.http
+      .get(url, { params: APIparams })
+      .pipe(tap((res) => console.log(res)))
+      .subscribe((el: any) => {
+        this.cities = el;
+      });
+  }
 }
