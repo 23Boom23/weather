2a3996637f4f85387e31bfd8823b4d946efafc61
garus_ae <garus.ae@weldbook.ru>
2021-12-20 01:25:04 +0300

    task behaviour subject

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { BehaviorSubject, filter } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'taskapi';
+export class AppComponent implements OnInit {
+  title = 'behavioursubject';
+
+  value = 0;
+  ngOnInit() {
+    const random = new BehaviorSubject(0);
+    function value() {
+      const rand = Math.round(Math.random() * 1000);
+      random.next(rand);
+    }
+    setInterval(value, 1000);
+
+    random
+      .pipe(filter((value: number) => value >= 300 && value <= 700))
+      .subscribe((el) => (this.value = el));
+  }
 }
