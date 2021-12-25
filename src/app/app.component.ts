49710982ed27777cc335eb9fd252dd232536e46e
garus_ae <garus.ae@weldbook.ru>
2021-12-25 13:08:16 +0300

    feat:add display wheter when click on city

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,7 +13,9 @@ export class AppComponent implements OnInit {
   temperature = '';
   pressure = '';
   humidity = '';
+  city: any;
   constructor(private http: HttpClient) {}
+  // 7f7cc2ea-568f-4dae-9358-12518c1b7c7d
 
   ngOnInit() {}
   // ipKey 7f7cc2ea-568f-4dae-9358-12518c1b7c7d
@@ -37,25 +39,29 @@ export class AppComponent implements OnInit {
       });
   }
 
-  onWeather() {
+  onWeather(event:any) {
+    this.city = event.target.textContent;
     const domain = 'http://api.airvisual.com';
     const endpoint = 'v2/city';
     const APIKey = '7f7cc2ea-568f-4dae-9358-12518c1b7c7d';
     const APIparams = {
-      city: 'Los Angeles',
+      city: `${this.city}`,
       state: 'California',
       country: 'USA',
       key: APIKey,
     };
 
     const url = `${domain}/${endpoint}`;
-    this.http
-      .get(url, { params: APIparams })
-      .pipe(map((res: any) => res.data.current.weather))
-      .subscribe((el: any) => {
-        this.temperature = el.tp;
-        this.humidity = el.hu;
-        this.pressure = el.pr;
-      });
+    this.http.get(url, { params: APIparams }).subscribe((el: any) => {
+      console.log(
+        '🚀 ~ file: app.component.ts ~ line 55 ~ AppComponent ~ this.http.get ~ el',
+        el
+      );
+
+      this.city = el.data;
+      this.temperature = el.data.current.weather.tp;
+      this.humidity = el.data.current.weather.hu;
+      this.pressure = el.data.current.weather.pr;
+    });
   }
 }
